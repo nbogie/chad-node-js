465bdb575ad497b9822f015cb6337326f3b8f0f7
Chad Allen <chadallenmagic@gmail.com>
2024-05-21 12:46:38 -0700

    update folder for review cardrandomizer and one.js

---
+++ src/card-randomizer.js
@@ -0,0 +1,6 @@
+import lodash from "lodash";
+
+const playingCards=lodash.sample(["the ace of spades", "the ace of hearts", "the ace of clubs", "the ace of diamonds", "the king of spades", "the king of hearts", "the king of clubs", "the king of diamonds".
+    e
+]);
+console.log(playingCards)
--- src/one.js
+++ src/one.js
@@ -2,15 +2,30 @@
 import lodash from "lodash";
 
 //Here are three arrays of strings: nouns, verbs, and adjectives
-const nouns = ["dog", "cat", "musician", "spaceship", "poem", "pirate", "bass"];
-const verbs = ["ate", "embraced", "destroyed", "hypnotised", "played"];
-const adjectives = [
-    "inquisitive",
-    "cybernetic",
-    "melodic",
-    "stealthy",
-    "kung-fu",
+const nouns = [
+    "barbarian",
+    "bard",
+    "cleric",
+    "druid",
+    "fighter",
+    "monk",
+    "paladin",
+    "ranger",
+    "rogue",
+    "sorcerer",
+    "warlock",
+    "wizard",
 ];
+const verbs = [
+    "attacks",
+    "kills",
+    "pummels",
+    "blesses",
+    "charms",
+    "disarms",
+    "saves",
+];
+const adjectives = ["strong", "agile", "noble", "stealthy", "masterful"];
 
 //We'll use the "sample" function from the lodash library to pick a word from each word array at random.
 const chosenNoun1 = lodash.sample(nouns);
