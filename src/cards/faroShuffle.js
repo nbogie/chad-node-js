bed79d53bb4c3c4ceeaae0dc14ca57e88d7297c5
nbogie <neillbogie@googlemail.com>
2024-05-30 08:57:44 +0100

    more on faro

--- src/cards/faroShuffle.js
+++ src/cards/faroShuffle.js
@@ -1,6 +1,6 @@
 let stack1 = createCards(1, 26);
 let stack2 = createCards(27, 52);
 
-let result = performFaroShuffle(stack1, stack2);
+let shuffledDeck = performFaroShuffle(stack1, stack2);
 
-logCardInPosition(1, result);
+let firstCard = logCardInPosition(1, shuffledDeck);
