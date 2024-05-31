ef648bf8659aa48e708f36298d9d80247b240d30
nbogie <neillbogie@googlemail.com>
2024-05-31 18:15:39 +0100

    put demo at start

--- src/cards/faroShuffle.js
+++ src/cards/faroShuffle.js
@@ -1,3 +1,15 @@
+let stack1 = createCards(1, 26);
+let stack2 = createCards(27, 52);
+
+let shuffledDeck = performFaroShuffle(stack1, stack2);
+
+logCardInPosition(1, shuffledDeck);
+logCardInPosition(2, shuffledDeck);
+logCardInPosition(3, shuffledDeck);
+logCardInPosition(50, shuffledDeck);
+logCardInPosition(51, shuffledDeck);
+logCardInPosition(52, shuffledDeck);
+
 function logCardInPosition(position, deck) {
     const card = deck[position - 1];
     console.log("At position ", position, " is ", card);
@@ -25,15 +37,3 @@ function performFaroShuffle(stackA, stackB) {
     }
     return resultingDeck;
 }
-
-let stack1 = createCards(1, 26);
-let stack2 = createCards(27, 52);
-
-let shuffledDeck = performFaroShuffle(stack1, stack2);
-
-logCardInPosition(1, shuffledDeck);
-logCardInPosition(2, shuffledDeck);
-logCardInPosition(3, shuffledDeck);
-logCardInPosition(50, shuffledDeck);
-logCardInPosition(51, shuffledDeck);
-logCardInPosition(52, shuffledDeck);
