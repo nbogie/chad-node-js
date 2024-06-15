a7b25e1c40ccf7b9c08945f691da5b30125cbfc5
Chad Allen <chadallenmagic@gmail.com>
2024-06-15 13:34:39 -0700

    added modified roll

--- src/readlineDemos/d-twenty-dice-roller.js
+++ src/readlineDemos/d-twenty-dice-roller.js
@@ -1,15 +1,18 @@
-//ask the dm for the number of dice to be rolled. Can be anywhere between 2 to 100.  
-//Report the value to the dm . 
-//Ask the dm if there are any bonuses to the roll. 
-//report result to the dm . 
-// Ask the dm the roll for success. 
-// if the opposing force is an attacker, make a random roll with a d20. 
-// If the first roll is greater than the second roll, ask the dm the damage to be applied to the attack. 
- //This damaged roll can either be a fixed number or a random roll. 
+//ask the dm for the number of dice to be rolled. Can be anywhere between 2 to 100.
+//Report the value to the dm .
+//Ask the dm if there are any bonuses to the roll.
+//report result to the dm .
+// Ask the dm the roll for success.
+// if the opposing force is an attacker, make a random roll with a d20.
+// If the first roll is greater than the second roll, ask the dm the damage to be applied to the attack.
+//This damaged roll can either be a fixed number or a random roll.
 
 import readline from "readline-sync";
-const dieToRoll =  readline.questionInt("What die? ");
-console.log ("The input number is " + dieToRoll);
-// generate a number between 1 and dieToRoll 
-const dieRolled = Math.ceil(dieToRoll * Math.random()); ;
+const dieToRoll = readline.questionInt("What die? ");
+console.log("The input number is " + dieToRoll);
+// generate a number between 1 and dieToRoll
+const dieRolled = Math.ceil(dieToRoll * Math.random());
 console.log("Rolled Outcome" + dieRolled);
+const bonusToAdd = readline.questionInt("Are there bonuses to add? ");
+const modifiedRoll = dieRolled + bonusToAdd;
+console.log("The modified roll is " + modifiedRoll);
